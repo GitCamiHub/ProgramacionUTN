7ac3b9761b1e60df4f38fd298a4c1e9f0b5688ed
Camila Irisarri <irisarricamila96@gmail.com>
2023-08-15 16:27:42 -0300

    Ejercicios complementarios 1

---
+++ ejerComp1.py
@@ -0,0 +1,71 @@
+# Ejercicio 1
+numero1 = 5
+print(numero1)
+
+# Ejercicio 2
+numero2 = 2.5
+print(numero2)
+
+# Ejercicio 3
+suma = numero1 + numero2
+print(suma)
+
+# Ejercicio 4
+resta = numero1 - numero2
+mult = numero1 * numero2
+div = numero1 / numero2
+print('Resta:',resta, ' Multiplicacion:',mult, ' Division',div)
+
+# Ejercicio 5
+nombre = 'Camila'
+print(nombre)
+
+# Ejercicio 6
+precio = 1600.5
+print(precio)
+
+# Ejercicio 7
+descuento = 0.25
+print(descuento)
+
+# Ejercicio 8
+precio_final = precio-precio*0.25
+print(precio_final)
+
+# Ejercicio 9
+cadena = 'Python es un lenguaje de programacion.'
+print(cadena)
+
+# Ejercicio 10
+longitud = len(cadena)
+print(longitud)
+
+# Ejercicio 11
+precio = int(56.24)
+print(precio)
+
+# Ejercicio 12
+nombre = 'Susana'
+apellido = 'Gimenez'
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+# Ejercicio 13
+edad = 26
+edad += 5
+edad -= 10
+print(edad)
+
+# Ejercicio 14
+altura = 1.70
+altura *= 4
+altura /= 3
+print(altura)
+
+# Ejercicio 15
+nombre_mayus = 'CAMILA'
+print(nombre_mayus.lower())
+
+# Ejercicio 16
+primera_mayus = nombre_mayus.lower().title()
+print(primera_mayus)
